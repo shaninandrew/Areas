609e22d87fc67cb50f5a7fb36ef2bc12e2125553
shaninandrew <hedgestandart@gmail.com>
2023-08-20 22:46:51 +0300

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,17 @@
+﻿
+-- Таблицы
+-- Products: id, name, type
+-- Category: id, category
+-- links: id, id_product, id_category
+
+Select p.name, c.category from Products as p 
+INNER JOIN links as l ON l.id_product = p.id
+LEFT OUTER JOIN category as c ON c.id = l.id_category
+GO
+
+-- с вариантом отбора
+Select p.name, c.category from Products as p 
+INNER JOIN links as l ON l.id_product = p.id
+LEFT OUTER JOIN category as c ON c.id = l.id_category
+WHERE p.name LIKE '%товара который ищем%'
+GO
